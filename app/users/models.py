7427bb2381ec67ee3952093193c85fa425222afb
unknown <Raisakulakovska@gmail.com>
2020-05-28 12:44:23 +0300

    app user

---
+++ app/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100, unique=True)
+    messages = models.CharField(max_length=1000, unique=True)
+    owner = models.ForeignKey(User, related_name='users', on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
